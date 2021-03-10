db06b8fd698b1c07d8e2ebe01ca1df3bcb97c99c
wuxiaobai24 <wuxiaobai24@foxmail.com>
2021-03-10 23:41:56 +0800

    fix bug

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/probberechts/hexo-theme-cactus.git themes/cactus 
-git clone https://${GITHUB_TOKEN}@github.com/wuxiaobai24/blog source/_post -b algo
+git clone https://${GITHUB_TOKEN}@github.com/wuxiaobai24/blog source/_posts -b algo
+ls source/_posts
